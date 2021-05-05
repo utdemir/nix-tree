42af89cb56b8a86ae5c2975790b8c486a0e5a4a9
Utku Demir <me@utdemir.com>
2021-05-05 23:23:34 +1200

    Stop using flakes

--- default.nix
+++ default.nix
@@ -1,3 +1,37 @@
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = ./.;
-}).defaultNix
+let
+  sources = import ./nix/sources.nix;
+in
+
+{ pkgs ? import sources.nixpkgs {} }:
+
+let
+  myHaskellPackages = pkgs.haskellPackages.override {
+    overrides = hself: hsuper: {
+      "nix-tree" =
+        hself.callCabal2nix "nix-tree" (builtins.fetchGit ./.) {};
+    };
+  };
+
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p."nix-tree"
+    ];
+    buildInputs = [
+      myHaskellPackages.haskell-language-server
+      pkgs.haskellPackages.cabal-install
+      pkgs.haskellPackages.ghcid
+      pkgs.haskellPackages.ormolu
+      pkgs.haskellPackages.hlint
+      pkgs.niv
+      pkgs.nixpkgs-fmt
+    ];
+    withHoogle = false;
+  };
+
+in
+{
+  inherit shell;
+  "nix-tree" =
+    pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."nix-tree");
+}
+
--- shell.nix
+++ shell.nix
@@ -1,3 +1,1 @@
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = ./.;
-}).shellNix
+(import ./. {}).shell
