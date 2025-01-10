2e48b17f17457509c5a2407f3b844425f3c9eabf
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2025-01-11 00:42:29 +0300

    Add $XDG_STATE_HOME/nix/profiles/home-manager to roots

--- src/NixTree/Main.hs
+++ src/NixTree/Main.hs
@@ -119,11 +119,13 @@ main = do
     [] -> do
       home <- getHomeDirectory
       nixXdgDirectory <- getXdgDirectory XdgState "nix/profile"
+      homeManagerDirectory <- getXdgDirectory XdgState "nix/profiles/home-manager"
       roots <-
         filterM
           isValidRoot
           [ home </> ".nix-profile",
             nixXdgDirectory,
+            homeManagerDirectory,
             "/var/run/current-system"
           ]
       case roots of
